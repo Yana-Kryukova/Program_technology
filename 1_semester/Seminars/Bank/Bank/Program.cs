7b122e23f0c68c6e22c3f5100807326de18f6fd7
Yana-Kryukova <krjanik@icloud.com>
2026-09-25 15:13:36 +0700

    #3 Тест

--- 1_semester/Seminars/Bank/Bank/Program.cs
+++ 1_semester/Seminars/Bank/Bank/Program.cs
@@ -5,9 +5,9 @@
         static void Main(string[] args)
         {
 
-            BankAccount account1 = new BankAccount("Yana", 100000);
+            BankAccount account1 = new BankAccount("Yana ", 100000);
             BankAccount account2 = new BankAccount("Lena", 10);
-            Console.WriteLine($"account {account1.Balance} №{account1.Number} {account1.Owner}");
+            Console.WriteLine($"account  {account1.Balance} №{account1.Number} {account1.Owner}");
             Console.WriteLine($"account {account2.Balance} №{account2.Number} {account2.Owner}");
 
             account1.MakeDeposit(2000000, DateTime.UtcNow, ":)");
